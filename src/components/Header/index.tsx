d49ef77f1bd16713e7d11ee80231688bb7540aa6
JUNIOR ALVES <jjalves96@gmail.com>
2021-08-28 20:14:51 -0300

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Header = () => {
+  return (
+    <S.Container>
+      <S.Content>Header</S.Content>
+    </S.Container>
+  );
+};
+
+export default Header;
