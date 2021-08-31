4a770f215ad06665e49f6bd719e285a603fd116d
Junior Alves <jjalves96@gmail.com>
2021-08-31 15:58:09 -0300

    chore: refactor Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { RectButtonProperties } from 'react-native-gesture-handler';
 
+import * as S from './styles';
+
 interface ButtonProps extends RectButtonProperties {
   children: React.ReactNode;
   title: string;
   color?: string;
   onPress: () => void;
 }
-
-import * as S from './styles';
 
 const Button = ({title, color = '', children, ...rest}: ButtonProps) => {
   return (
