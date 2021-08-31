58f31b80a7f2db82b86d71e47b82e3240d2894c9
JUNIOR ALVES <jjalves96@gmail.com>
2021-08-30 22:35:15 -0300

    style: add new colors into theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,15 +1,17 @@
 export default { 
   colors: { 
-    primary: '#5636d3', 
-    secondary: '#ff872c', 
+    primary: '#162736', 
+    secondary: '#FA8131', 
     secondary_light: 'rgba(255, 135, 44, 0.3)',  
-    sucess: '#12a454', 
+    sucess: '#20C970', 
     sucess_light: 'rgba(18, 168, 84, 0.5)',  
     attention: '#e83f5b', 
     attention_light: 'rgba(232, 63, 91, 0.5)',  
-    shape: '#fff', 
+    shape: '#FFFFFF', 
     title: '363f5f', 
     text: '#969cb2', 
-    background: '#f0f2f5' 
+    background: '#162736' ,
+    item: '#203449',
+    disabled: '#707070'
   }
 }
