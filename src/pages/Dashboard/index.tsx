15b09d295de068119b70108b51327d7ede59a448
JUNIOR ALVES <jjalves96@gmail.com>
2021-08-30 22:36:23 -0300

    feat: create Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import Button from '../../components/Button';
+import { StatusBar } from 'react-native';
+
 import Header from '../../components/Header';
+import ItemList from '../../components/ItemList';
+import MenuBottom from '../../components/MenuBottom';
+
+import { ItemsData } from '../../components/ItemList/data';
 
 import * as S from './styles';
 
 const Dashboard = () => {
   return (
-    <S.Container>
-      <Header />
-      <Button>
-        Texto
-      </Button>
-    </S.Container>
+    <>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header title="Extra Forte" />
+
+      <S.Container>
+        {ItemsData.map((item)=> (
+          <ItemList name={item.name} unity={item.unity} price={item.price} image={item.path} />
+        ))}
+      </S.Container>
+      
+      <MenuBottom />
+    </>
   )
 }
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,9 +1,13 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+  ${({ theme }) => css`
+    flex: 1;
+    
+    align-items: center;
 
-  padding: 0 24px;
+    padding: 0 24px;
+
+    background-color: ${theme.colors.background};
+  `}
 `;
