7116a58f97302f2f26b0fa87e8606bc85152ac4d
JUNIOR ALVES <jjalves96@gmail.com>
2021-08-31 08:59:51 -0300

    chore: add new styles for MenuButton component

--- src/components/MenuBottom/styles.ts
+++ src/components/MenuBottom/styles.ts
@@ -1,26 +1,42 @@
-import styled from 'styled-components/native';
-import {RectButton} from 'react-native-gesture-handler';
+import styled, { css } from 'styled-components/native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface ButtonProps extends RectButtonProps {
+  color?: string;
+}
 
 export const Container = styled.View`
-  padding-top: 8px;
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-bottom: 20px;
-  bottom: 0;
-  flex-direction: row;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-  border-top-width: 1px;
-  border-top-color: #213c54;
-  background-color: #162736;
-  height: 100px;
+  ${({ theme }) => css`
+    bottom: 0;
+    
+    flex-direction: row;
+    
+    width: 100%;
+    
+    align-items: center;
+    justify-content: space-between;
+    
+    border-top-width: 1px;
+    border-top-color: ${theme.colors.item};
+    
+    background-color: ${theme.colors.background};
+    
+    height: ${RFValue(100)}px;
+
+    padding: 24px 24px ${getBottomSpace() + 24}px;
+  `}
 `;
 
-export const ItemContainer = styled(RectButton)`
-  width: 120px;
-  flex-direction: row;
-  align-items: center;
+export const ItemContainer = styled(RectButton)<ButtonProps>`
+  ${({ theme }) => css`
+    width: 120px;
+    flex-direction: row;
+    align-items: center; 
+
+    /* color: ${theme.colors.sucess}; */
+  `}
 `;
 
 export const Image = styled.Image`
@@ -35,21 +51,31 @@ export const ItemInfo = styled.View`
 `;
 
 export const QuantityTitle = styled.Text`
-  font-size: 14px;
-  color: #20c970;
+  ${({ theme }) => css`
+    font-size: ${RFValue(14)}px;
+    color: ${theme.colors.sucess};
+  `}
 `;
 
 export const QuantityValue = styled.Text`
-  font-size: 16px;
-  font-weight: bold;
-  color: #20c970;
+  ${({ theme }) => css`
+    font-size: ${RFValue(16)}px;
+    font-family: ${theme.fonts.primary_700};
+
+    color: ${theme.colors.sucess};
+  `}
 `;
 
-export const AddButton = styled(RectButton)`
-  width: 64px;
-  height: 64px;
-  border-radius: 30px;
-  background-color: #fa8231;
-  align-items: center;
-  justify-content: center;
+export const AddButton = styled.View`
+  ${({ theme }) => css`
+    width: ${RFValue(64)}px;
+    height: ${RFValue(64)}px;
+
+    /* border-radius: 30px;
+    
+    background-color: ${theme.colors.secondary};
+    
+    align-items: center;
+    justify-content: center;   */
+  `}
 `;
