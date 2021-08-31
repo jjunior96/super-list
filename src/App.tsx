2add6aafccf43b9074338f4cb26348906224d3a8
JUNIOR ALVES <jjalves96@gmail.com>
2021-08-30 22:36:49 -0300

    chore: add expo-font config

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { ThemeProvider } from 'styled-components';
 
+import AppLoading from 'expo-app-loading';
+import { useFonts,  Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
 import theme from './global/styles/theme';
 import Dashboard from './pages/Dashboard';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ 
+    Roboto_400Regular,
+    Roboto_700Bold
+  }); 
+  
+  if (!fontsLoaded) { 
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
