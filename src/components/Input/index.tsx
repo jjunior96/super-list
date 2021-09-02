3456130b91248c8babdd8c89285b93a4d7aaf69c
JUNIOR ALVES <jjalves96@gmail.com>
2021-09-02 05:28:48 -0300

    chore: add new props into component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef} from 'react';
-import {TextInputProps} from 'react-native';
-import { useField } from '@unform/core';
+import { TextInputProps } from 'react-native';
+import { useTheme } from 'styled-components';
 
 import * as S from './styles';
 
@@ -14,27 +14,10 @@ interface InpuValueRef {
 }
 
 const Input = ({name, icon, ...rest}: InputProps) => {
-  const {registerField, defaultValue, fieldName, error} = useField(name);
-  const inputValueRef = useRef<InpuValueRef>({value: defaultValue});
-
-  useEffect(() => {
-    registerField({
-      name: fieldName,
-      ref: inputValueRef.current.value,
-      path: 'value',
-    });
-  }, [fieldName, registerField]);
+  const theme = useTheme();
 
   return (
-    <S.Container>
-      <S.TextInput
-        keyboardAppearance="dark"
-        placeholderTextColor="#62707F"
-        defaultValue={defaultValue}
-        onChangeText={(value) => (inputValueRef.current.value = value)}
-        {...rest}
-      />
-    </S.Container>
+    <S.Container placeholderTextColor={theme.colors.text} {...rest} />
   );
 };
 
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,26 +1,23 @@
 import styled, { css } from 'styled-components/native';
+import { TextInput } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View`
+
+export const Container = styled(TextInput)`
   ${({ theme }) => css`
     width: 100%;
     height: 60px;
 
-    padding: 0 16px;
-    margin-bottom: 16px;
+    padding: 0 ${RFValue(16)}px;
+    margin-bottom: ${RFValue(16)}px;
     
     background-color: ${theme.colors.item};
     
     border-radius: 12px;
-  `}
-`;
-
-export const TextInput = styled.TextInput`
-  ${({ theme }) => css`
-    flex: 1;
     
     color: ${theme.colors.shape};
     
-    font-size: 16px;
+    font-size: ${RFValue(16)}px;
     font-family: ${theme.fonts.primary_400};
   `}
 `;
