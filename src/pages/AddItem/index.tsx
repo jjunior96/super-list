7f6c71ead7bee8b3a5315fa8a27e0f81e3f31ddd
JUNIOR ALVES <jjalves96@gmail.com>
2021-09-02 09:22:57 -0300

    chore: add margin for header

--- src/pages/AddItem/index.tsx
+++ src/pages/AddItem/index.tsx
@@ -1,13 +1,60 @@
-import React from 'react';
-import { StatusBar, KeyboardAvoidingView, Keyboard } from 'react-native';
-import Button from '../../components/Button';
+import React, { useState } from 'react';
+import { StatusBar, Modal, KeyboardAvoidingView, Keyboard } from 'react-native';
+import { useForm } from 'react-hook-form';
 
+import AddToCartButton from '../../components/AddToCartButton';
+import Button from '../../components/Button';
 import Header from '../../components/Header';
-import Input from '../../components/Input';
+import InputControlled from '../../components/InputControlled';
+import SelectButton from '../../components/SelectButton';
+import SelectUnityButton from '../../components/SelectUnityButton';
 
 import * as S from './styles';
 
+interface FormDataProps {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
 const AddItem = () => {
+  const [isAddToCart, setIsAddToCart] = useState(false);
+  const [isOpenSelectModal, setIsOpenSelectModal] = useState(false);
+
+  const { control, handleSubmit } = useForm();
+  
+  const [unity, setUnity] = useState({
+    key: 'unity',
+    name: 'Unidade'
+  });
+
+  function handleAddToCart() {
+    setIsAddToCart(!isAddToCart);
+  }
+
+  function handleOpenSelectModal() {
+    setIsOpenSelectModal(true);
+  }
+
+  function handleCloseSelectModal() {
+    setIsOpenSelectModal(false);
+  }
+
+  function handleSubmitForm(form: FormDataProps) {
+    const { name, price, quantity } = form;
+
+    const data = {
+      name, 
+      price, 
+      quantity, 
+      unity, 
+      isAddToCart
+    }
+
+    console.log(data);
+  }
+
+
   return (
     <>
       <StatusBar 
@@ -20,15 +67,31 @@ const AddItem = () => {
       <S.Container>
        <S.Form>
          <S.FieldsForm>
-          <Input name="nome" placeholder="Nome" />
+          <InputControlled control={control} name="name" placeholder="Nome" />
           
-          <Input name="price" placeholder="Preço" keyboardType="numeric" />
+          <InputControlled control={control} name="price" placeholder="Preço" keyboardType="numeric" />
           
-          <Input name="quantity" placeholder="Quantidade" keyboardType="numeric" />
+          <InputControlled control={control} name="quantity" placeholder="Quantidade" keyboardType="numeric" />
+
+          <SelectButton title={unity.name} onPress={handleOpenSelectModal} />
+
+          <AddToCartButton title="Add ao carrinho" isChecked={isAddToCart} onPress={handleAddToCart} />
          </S.FieldsForm>
 
-        <Button>Adicionar</Button>
+          <Button
+            onPress={handleSubmit(handleSubmitForm)}
+          >
+            Adicionar
+          </Button>
        </S.Form>
+
+       <Modal visible={isOpenSelectModal} >
+        <SelectUnityButton 
+          unity={unity}
+          setUnity={setUnity}
+          closeSelectUnity={handleCloseSelectModal}
+        />  
+       </Modal>
       </S.Container>
     </>
   )
