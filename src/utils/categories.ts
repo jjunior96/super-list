fe181b94fde2092a09e0098acf04135b6654a3a7
JUNIOR ALVES <jjalves96@gmail.com>
2021-09-02 09:21:21 -0300

    feat: create categories file

---
+++ src/utils/categories.ts
@@ -0,0 +1,5 @@
+export const unityCategories = [
+    { key: 'kg', name: 'Kg', color: '#5636D3' },
+    { key: 'un', name: 'Un', color: '#FF872C' },
+    { key: 'l', name: 'L', color: '#12A454' },
+]; 
