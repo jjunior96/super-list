ab7fe013242de92b5e04dce24874b920f5f39cb2
JUNIOR ALVES <jjalves96@gmail.com>
2021-09-04 08:59:27 -0300

    chore: clean up component

--- src/components/SelectButton/index.tsx
+++ src/components/SelectButton/index.tsx
@@ -3,7 +3,7 @@ import { TouchableOpacityProps } from 'react-native';
 
 import * as S from './styles';
 
-interface SelectButtonProps extends  TouchableOpacityProps {
+interface SelectButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
